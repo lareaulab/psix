9c7ecd0ae3a00d740612b1a06b86a60a893e84e6
Carlos F Buen Abad Najar <cfbuenabadn@berkeley.edu>
2023-08-17 23:27:06 -0500

    & --> .intersection()

--- psix/solo2psi.py
+++ psix/solo2psi.py
@@ -124,7 +124,7 @@ def get_psi_table_solo(intron_mtx_exons, minJR=5, minCell=20, tenX = False):
     events_i2 = pd.Index([x[:-3] for x in intron_mtx_exons.index if '_I2' in x])
     events_se = pd.Index([x[:-3] for x in intron_mtx_exons.index if '_SE' in x])
     
-    events = events_i1 & events_i2 & events_se
+    events = events_i1.intersection(events_i2).intersection(events_se)
     
     
     i1_events = [x + '_I1' for x in events]
@@ -143,7 +143,7 @@ def get_psi_table_solo(intron_mtx_exons, minJR=5, minCell=20, tenX = False):
     I2_filt = I2_table.index[(I2_table > minJR).sum(axis=1) > minCell]
     SE_filt = SE_table.index[(SE_table > minJR).sum(axis=1) > minCell]
     
-    filtered_events = I1_filt & I2_filt & SE_filt
+    filtered_events = I1_filt.intersection(I2_filt).intersection(SE_filt)
     
     I1_table = I1_table.loc[filtered_events]
     I2_table = I2_table.loc[filtered_events]
@@ -184,7 +184,7 @@ def solo_to_psi(
 
     alt_exons = psi.index[np.abs(0.5 - psi.mean(axis=1)) <= (0.5-minPsi)]
     obs_exons = psi.index[psi.isna().mean(axis=1) <= 1-min_observed]
-    selected_exons = alt_exons & obs_exons
+    selected_exons = alt_exons.intersection(obs_exons)
 
     psi = psi.loc[selected_exons]
     reads = reads.loc[selected_exons]
@@ -204,14 +204,14 @@ def solo_to_psi(
             cell_list = psi.columns
         mrna = tpm2mrna(tpm_file, cell_list)
         ##### New thing
-        cells = psi.columns & mrna.columns
+        cells = psi.columns.intersection(mrna.columns)
         mrna = mrna[cells]
         psi = psi[cells]
 
         mrna_per_event = get_mrna_per_event(mrna, psi, reads, intron_mtx_CI, solo=True) #constitutive_sj_file)
 
     if len(self.adata.obs) > 0:
-        idx = self.adata.obs.index & mrna_per_event.index
+        idx = self.adata.obs.index.intersection(mrna_per_event.index)
     else:
         idx = mrna_per_event.index
 
